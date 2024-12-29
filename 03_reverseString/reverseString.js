7ec7503c112bd4709ed80bd3d06e257cd7a4b739
bpm <74379183+superliminal003@users.noreply.github.com>
2024-12-29 13:45:52 +0300

    03 reverse string done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	let newstr = "";
+	for (let i = str.length; i >= 0; i--) {
+		newstr = newstr + (str.charAt(i));
+	}
+	return newstr;
 };
 
 // Do not edit below this line
