a6b408a3336df5c9eba0f77a7705c0b732f9a36f
bpm <74379183+superliminal003@users.noreply.github.com>
2024-12-30 07:12:18 +0300

    06 leap years done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(yearin) {
+	if (((yearin % 4 == 0) && !(yearin % 100 == 0)) || (yearin % 400 == 0)) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
