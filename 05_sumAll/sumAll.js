3d02ce144518711f3140b9943fa360f0c01c0664
bpm <74379183+superliminal003@users.noreply.github.com>
2024-12-30 07:05:59 +0300

    05 sum all done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+	if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+		return "ERROR";
+	}
+	if (a < b) {
+	} else if (a > b) {
+		let d = b;
+		b = a;
+		a = d;
+	}
+	let c = 0;
+	for (let i = a; i <= b; i++) {
+		c += i;
+	}
+	return c;
 };
 
 // Do not edit below this line
