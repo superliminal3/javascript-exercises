81e1d28bd56adf9571123c7afc1f8a3b8d8fa639
bpm <74379183+superliminal003@users.noreply.github.com>
2024-12-30 07:23:45 +0300

    07 temp conversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+	celsiusTemp = (fahrTemp - 32) * (5/9);
+	return parseFloat(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+	fahrTemp = (celsiusTemp * (9/5) + 32);
+	return parseFloat(fahrTemp.toFixed(1));
 };
 
 // Do not edit below this line
