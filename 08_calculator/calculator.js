7bbb113242de7a56fb761c5b6531daa1faeaac12
bpm <74379183+superliminal003@users.noreply.github.com>
2025-01-05 15:12:16 +0300

    08 calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(((acc, val) => acc + val), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce(((acc,val) => acc * val), 1);
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+	let num2 = num1;
+	for (let i = 1; i < power; i++) {
+		num2 = num2 * num1;
+	}
+	return num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let n = 1;
+	for (let i = 1; i <= num; i++) {
+		n = n * i;
+	}
+	return n;
 };
 
 // Do not edit below this line
