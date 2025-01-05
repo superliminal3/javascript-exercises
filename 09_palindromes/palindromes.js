d329b89a5d5034bdd32aa34612c6c005a9a42d14
bpm <74379183+superliminal003@users.noreply.github.com>
2025-01-05 17:31:01 +0300

    09 palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const isAValidChar = function (c) {
+	if (((c.toLowerCase() != c.toUpperCase()) || !(isNaN(c))) && c != " ") {
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+const palindromes = function (string) {
+	let stringArrFiltered = string.split("").filter(isAValidChar); 
+	let middlePoint = (stringArrFiltered.length / 2).toFixed(0);
+
+	if (stringArrFiltered.length % 2 != 0) {
+		middlePoint--;
+		stringArrFiltered.splice((middlePoint), 1);
+	}
+
+	half1 = stringArrFiltered.slice(0, middlePoint).reverse().join().toLowerCase();
+	half2 = stringArrFiltered.slice(middlePoint, (stringArrFiltered.length + 1)).join().toLowerCase();
+
+	if (half1.includes(half2)) {
+		return true;
+	} else {
+		return false;
+	}
+
+
 
 };
 
