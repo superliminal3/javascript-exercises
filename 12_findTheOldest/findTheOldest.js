8d0cd46711106acc7f3dfebe3cf16866e5ced89e
bpm <74379183+superliminal003@users.noreply.github.com>
2025-01-08 12:45:06 +0300

    12 find the oldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	currentTime = new Date().getFullYear();
 
+	people.map((people) => {
+		if (!people.yearOfDeath) {
+			people.yearOfDeath = currentTime;
+		}
+		return people.age = people.yearOfDeath - people.yearOfBirth
+	});
+
+	people.sort((a, b) => parseFloat(b.age) - parseFloat(a.age));
+	return people[0];
 };
 
 // Do not edit below this line
